ebd8048e83e1d30df55a189bd3c459c748b0b516
yohannes2025 <185436086+yohannes2025@users.noreply.github.com>
2026-07-17 20:42:44 +0200

    Add backend Django project structure and core apps

---
+++ backend/ctalog/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CtalogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ctalog'
---
+++ backend/tenanats/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TenanatsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tenanats'
